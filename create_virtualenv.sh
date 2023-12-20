1e5d46f02b136f5d118c5249aca77af728e196a4
Josh Mooney <josh.mooney@Scopely-30256.local>
2023-12-20 23:40:59 +0000

    modified the create_virtualenv.sh to support new laptop

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -2,8 +2,9 @@
 
 # This is not in use right now
 set -e
+pip install virtualenv
+virtualenv -p python3 env
 
-virtualenv -p python3.8 env
 source env/bin/activate
 
 pip install --upgrade -r requirements.txt
